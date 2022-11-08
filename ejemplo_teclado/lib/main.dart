8de967c0481ec49856dfd60d809f10d8f4bc4336
ddelarosae <ddelarosae@uninorte.edu.co>
2022-11-08 10:22:35 -0500

    proyecto input teclado

---
+++ ejemplo_teclado/lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculadora NRC2287',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Teclado(),
+    );
+  }
+}
+
+class Teclado extends StatelessWidget {
+  const Teclado({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () {
+          FocusManager.instance.primaryFocus?.unfocus();
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                /*Image.asset(
+                  'assets/images/logo.png',
+                  scale: 12,
+                ),*/
+                const SizedBox(
+                  width: 12,
+                ),
+                const Text(
+                  'Proyecto Teclado NRC2287',
+                  style: TextStyle(color: Colors.red),
+                )
+              ],
+            ),
+          ),
+          body: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(labelText: "digite un numero"),
+                keyboardType: TextInputType.number,
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              ),
+              TextField(
+                decoration:
+                    InputDecoration(labelText: "digite un numero de telefono"),
+                keyboardType: TextInputType.phone,
+              ),
+              TextField(
+                decoration:
+                    InputDecoration(labelText: "digite un numero decimal"),
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              )
+            ],
+          ),
+        ));
+  }
+}
