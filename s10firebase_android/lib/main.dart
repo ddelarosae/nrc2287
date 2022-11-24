26ea7df9c6e2c55cdfeaf88cee607f9b30900fe4
ddelarosae <ddelarosae@uninorte.edu.co>
2022-11-24 10:56:16 -0500

    ejercicio crud

--- s10firebase_android/lib/main.dart
+++ s10firebase_android/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:s10firebase_android/model/user.dart';
+import 'package:s10firebase_android/page/user_page.dart';
 
-void main() async {
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
@@ -12,35 +14,95 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Firebase',
-      home: AppData(),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Firebase Read and Write',
+        theme: ThemeData(
+          primarySwatch: Colors.brown,
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              minimumSize: const Size.fromHeight(40),
+              textStyle: const TextStyle(fontSize: 20),
+            ),
+          ),
+        ),
+        home: const MainPage(),
+      );
 }
 
-class AppData extends StatelessWidget {
-  const AppData({super.key});
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        title: const Text('App Read NRC2287'),
-      ),
-      body: Center(
-        child: FloatingActionButton(
-          backgroundColor: Colors.blue,
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  final controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text('Usuarios NRC2287'),
+        ),
+        body: buildUsers(),
+        floatingActionButton: FloatingActionButton(
           child: const Icon(Icons.add_box_outlined),
           onPressed: () {
-            FirebaseFirestore.instance
-                .collection('frutas')
-                .add({'color': 'rojo'});
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => UserPage(),
+              ),
+            );
           },
         ),
-      ),
-    );
+      );
+  Widget buildUsers() => StreamBuilder<List<User>>(
+        stream: readUsers(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text('Hay un error ${snapshot.error}');
+          } else if (snapshot.hasData) {
+            final users = snapshot.data!;
+            return ListView(
+              children: users.map(buildUser).toList(),
+            );
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      );
+
+  Widget buildUser(User user) => ListTile(
+        leading: CircleAvatar(child: Text('${user.age}')),
+        title: Text(user.name),
+        subtitle: Text(user.birthday.toIso8601String()),
+      );
+
+  Stream<List<User>> readUsers() => FirebaseFirestore.instance
+      .collection('users')
+      .snapshots()
+      .map((snapshot) =>
+          snapshot.docs.map((doc) => User.fromJson(doc.data())).toList());
+
+  Future<User?> readUser() async {
+    final docUser = FirebaseFirestore.instance.collection('users').doc('my-id');
+    final snapshot = await docUser.get();
+
+    if (snapshot.exists) {
+      return User.fromJson(snapshot.data()!);
+    }
+  }
+
+  Future createUser({required String name}) async {
+    final docUser = FirebaseFirestore.instance.collection('users').doc();
+
+    final json = {
+      'id': docUser.id,
+      'name': name,
+      'age': 28,
+      'birthday': DateTime(2022, 11, 24),
+    };
+
+    await docUser.set(json);
   }
 }
